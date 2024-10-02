695ddd34a7ac29369c2665eb63945bf5c09207e3
jennmcneill <jennifermcneill28@gmail.com>
2024-10-02 11:37:40 -0400

    Revert "switch for loop to while loop"
    This reverts commit 4b8cff0fb05cc28400cc97c9f40ede0ed9939181.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -15,14 +15,16 @@ file_name = './data/raw/sara.txt'
 #Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file line by line
-lineString = file_object.readline()
+#Read contents of file into a list
+line_list = file_object.readlines()
 
-#Pretend we read one line of data from the file #While loop remains active as long as there is a new line
-while lineString:
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+for lineString in line_list:
     #Check if line is a data line
     if lineString[0] in ("#","u"):
-        lineString = file_object.readline()
         continue
     
     #Split the string into a list of data items
@@ -37,6 +39,3 @@ while lineString:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    #Read next line
-    lineString = file_object.readline()
